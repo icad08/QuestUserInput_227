7d50e44bbefc86bed36b498ee37a20d77444cf10
icad08 <irsyadrm08@gmail.com>
2025-11-01 08:03:42 +0700

    update main activity lagi

--- app/src/main/java/com/example/inputuser/MainActivity.kt
+++ app/src/main/java/com/example/inputuser/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.lightColors
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,18 +22,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-// Theme ini dari kode kita sebelumnya, letakkan di file ini
-// atau di file Theme.kt Anda sendiri.
-@Composable
-fun FormulirPendaftaranTheme(content: @Composable () -> Unit) {
-    MaterialTheme(
-        colors = lightColors(
-            primary = Color(0xFF673AB7),
-            background = Color(0xFFF3EFFF),
-            surface = Color.White
-        ),
-        content = content
-    )
 }
